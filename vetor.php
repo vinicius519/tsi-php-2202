59abc9d752ffdd3ae04be3d13b3aca0714428920
VINICIUS GABRIEL FERNANDES DA SILVA <vinicius.gfsilva1@senacsp.edu.br>
2022-08-31 22:20:57 -0300

    vetores

---
+++ vetor.php
@@ -0,0 +1,41 @@
+<?php
+
+//vetor
+
+$diaSemana = [ 0=> 'domingo' , 1=> 'segunda' , 2=> 'terca' ,3=> 'quarta' , 4=> 'quinta' , 5=> 'sexta' , 6=> 'sabado'];
+ 
+//echo "<pre>";
+
+//var_dump($diaSemana);
+
+$hoje = date('w');
+
+
+
+echo "hoje é {$diaSemana[$hoje]} <br><br>"; // interpolaçao com vetor precisa das {}
+
+
+$aluno = [ 0 => [ 'matricula' => 634545, 'nome' => 'joao' , 'semestre' =>2],
+                    1 =>['matricula' => 8548,'nome' => 'paulo', 'semestre' =>3],
+                    2 => ['matricula' => 65422, 'nome'=> 'Maria', 'semestre' =>1]];
+
+echo '<table border = "1">
+        <tr>
+        <td> Matricula</td>
+        <td>Nome</td>
+        <td>Semestre</td>
+        </tr>';
+
+for( $i = 0; $i < count($aluno) ; $i++){
+
+echo "<tr>
+            <td>{$aluno[$i]['matricula']}</td>
+            <td>{$aluno[$i]['nome']}</td>
+            <td>{$aluno[$i]['semestre']}</td>
+        </tr>";
+
+
+}       
+echo '<table>';
+
+
